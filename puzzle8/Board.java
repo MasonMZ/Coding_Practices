4bb63a4a5b135919de1c4d3fec8cb20788f8c307
Mason <minzhengcsu@gmail.com>
2022-11-18 20:47:42 -0500

    modified Board

--- puzzle8/Board.java
+++ puzzle8/Board.java
@@ -4,6 +4,8 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Arrays;
 
 public final class Board {
@@ -28,10 +30,11 @@ public final class Board {
         for (int i = 0; i < size; i += 1) {
             String newLine = "";
             for (int j = 0; j < size; j += 1) {
-                newLine = String.format(newLine + "  %d", squares[i][j]);
+                newLine = String.format(newLine + "%2d", squares[i][j]);
             }
             res = String.format(res + "%n" + newLine);
         }
+        res = String.format(res + "%n");
         return res;
     }
 
@@ -40,12 +43,12 @@ public final class Board {
         return size;
     }
 
-    // sum of Manhattan distances between tiles and goal
-    public int manhattan() {
+    // number of tiles out of place
+    public int hamming() {
         int res = 0;
         for (int i = 0; i < size; i += 1) {
             for (int j = 0; j < size; j += 1) {
-                if ((i * size + j + 1) != squares[i][j]) {
+                if ((i * size + j + 1) != squares[i][j] && !(i == size - 1 && j == size - 1)) {
                     res += 1;
                 }
             }
@@ -53,9 +56,24 @@ public final class Board {
         return res;
     }
 
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan() {
+        int score = 0;
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                if (squares[i][j] != 0) {
+                    score += Math.abs((squares[i][j] - 1) / size - i) + Math.abs(
+                            (squares[i][j] - 1) % size - j);
+                }
+
+            }
+        }
+        return score;
+    }
+
     // is this board the goal board?
     public boolean isGoal() {
-        int res = manhattan();
+        int res = hamming();
         return (res == 0);
     }
 
@@ -88,7 +106,61 @@ public final class Board {
         if (isUpperRightCorner()) {
             return upperRightNeighbors();
         }
+        if (isLowerLeftCorner()) {
+            return lowerLeftNeighbors();
+        }
+        if (isLowerRightCorner()) {
+            return lowerRightNeighbors();
+        }
+        if (isUpperSide()) {
+            return upperSideNeighbors();
+        }
+        if (isLowerSide()) {
+            return lowerSideNeighbors();
+        }
+        if (isLeftSide()) {
+            return leftSideNeighbors();
+        }
+        if (isRightSide()) {
+            return rightSideNeighbors();
+        }
+        return innerNeighbors();
+    }
 
+    // a board that is obtained by exchanging any pair of tiles
+    public Board twin() {
+        int n1 = StdRandom.uniformInt(size * size);
+        int row1 = n1 / size;
+        int col1 = n1 % size;
+        while (squares[row1][col1] == 0) {
+            n1 = StdRandom.uniformInt(size * size);
+            row1 = n1 / size;
+            col1 = n1 % size;
+        }
+        int n2 = StdRandom.uniformInt(size * size);
+        int row2 = n2 / size;
+        int col2 = n2 % size;
+        while (n1 == n2 || squares[row2][col2] == 0) {
+            n2 = StdRandom.uniformInt(size * size);
+            row2 = n2 / size;
+            col2 = n2 % size;
+        }
+        int[][] newTiles = new int[size][size];
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                if (i == row1 && j == col1) {
+                    newTiles[i][j] = squares[row2][col2];
+                }
+                else if (i == row2 && j == col2) {
+                    newTiles[i][j] = squares[row1][col1];
+                }
+                else {
+                    newTiles[i][j] = squares[i][j];
+                }
+            }
+        }
+        Board res = new Board(newTiles);
+        return res;
     }
 
     private Boolean isUpperLeftCorner() {
@@ -141,8 +213,46 @@ public final class Board {
         return (this.squares[size - 1][0] == 0);
     }
 
+    private Iterable<Board> lowerLeftNeighbors() {
+        Board[] neighbors = new Board[2];
+        int[][] newTiles = new int[size][size];
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                newTiles[i][j] = squares[i][j];
+            }
+        }
+        newTiles[size - 1][1] = 0;  // move left
+        newTiles[size - 1][0] = squares[size - 1][1];
+        neighbors[0] = new Board(newTiles);
+
+        newTiles[size - 1][1] = squares[size - 1][1]; // recover
+        newTiles[size - 2][0] = 0; // move down
+        newTiles[size - 1][0] = squares[size - 2][0];
+        neighbors[1] = new Board(newTiles);
+        return Arrays.asList(neighbors);
+    }
+
     private Boolean isLowerRightCorner() {
         return (this.squares[size - 1][size - 1] == 0);
+    }
+
+    private Iterable<Board> lowerRightNeighbors() {
+        Board[] neighbors = new Board[2];
+        int[][] newTiles = new int[size][size];
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                newTiles[i][j] = squares[i][j];
+            }
+        }
+        newTiles[size - 1][size - 2] = 0;  // move right
+        newTiles[size - 1][size - 1] = squares[size - 1][size - 2];
+        neighbors[0] = new Board(newTiles);
+
+        newTiles[size - 1][size - 2] = squares[size - 1][size - 2]; // recover
+        newTiles[size - 2][size - 1] = 0; // move up
+        newTiles[size - 1][size - 1] = squares[size - 2][size - 1];
+        neighbors[1] = new Board(newTiles);
+        return Arrays.asList(neighbors);
     }
 
     private Boolean isUpperSide() {
@@ -154,6 +264,37 @@ public final class Board {
         return false;
     }
 
+    private Iterable<Board> upperSideNeighbors() {
+        Board[] neighbors = new Board[3];
+        int[][] newTiles = new int[size][size];
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                newTiles[i][j] = squares[i][j];
+            }
+        }
+        int rowID = 0;
+        int colID = 0;
+        for (int i = 1; i < size - 1; i += 1) {
+            if (squares[rowID][i] == 0) {
+                colID = i;
+            }
+        }
+        newTiles[rowID][colID - 1] = 0;  // move right
+        newTiles[rowID][colID] = squares[rowID][colID - 1];
+        neighbors[0] = new Board(newTiles);
+
+        newTiles[rowID][colID - 1] = squares[rowID][colID - 1]; // recover
+        newTiles[rowID][colID + 1] = 0; // move left
+        newTiles[rowID][colID] = squares[rowID][colID + 1];
+        neighbors[1] = new Board(newTiles);
+
+        newTiles[rowID][colID + 1] = squares[rowID][colID + 1]; // recover
+        newTiles[rowID + 1][colID] = 0; // move up
+        newTiles[rowID][colID] = squares[rowID + 1][colID];
+        neighbors[2] = new Board(newTiles);
+        return Arrays.asList(neighbors);
+    }
+
     private Boolean isLowerSide() {
         for (int i = 1; i < size - 1; i += 1) {
             if (this.squares[size - 1][i] == 0) {
@@ -161,6 +302,37 @@ public final class Board {
             }
         }
         return false;
+    }
+
+    private Iterable<Board> lowerSideNeighbors() {
+        Board[] neighbors = new Board[3];
+        int[][] newTiles = new int[size][size];
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                newTiles[i][j] = squares[i][j];
+            }
+        }
+        int rowID = size - 1;
+        int colID = 0;
+        for (int i = 1; i < size - 1; i += 1) {
+            if (squares[rowID][i] == 0) {
+                colID = i;
+            }
+        }
+        newTiles[rowID][colID - 1] = 0;  // move right
+        newTiles[rowID][colID] = squares[rowID][colID - 1];
+        neighbors[0] = new Board(newTiles);
+
+        newTiles[rowID][colID - 1] = squares[rowID][colID - 1]; // recover
+        newTiles[rowID][colID + 1] = 0; // move left
+        newTiles[rowID][colID] = squares[rowID][colID + 1];
+        neighbors[1] = new Board(newTiles);
+
+        newTiles[rowID][colID + 1] = squares[rowID][colID + 1]; // recover
+        newTiles[rowID - 1][colID] = 0; // move down
+        newTiles[rowID][colID] = squares[rowID - 1][colID];
+        neighbors[2] = new Board(newTiles);
+        return Arrays.asList(neighbors);
     }
 
     private Boolean isLeftSide() {
@@ -172,6 +344,37 @@ public final class Board {
         return false;
     }
 
+    private Iterable<Board> leftSideNeighbors() {
+        Board[] neighbors = new Board[3];
+        int[][] newTiles = new int[size][size];
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                newTiles[i][j] = squares[i][j];
+            }
+        }
+        int rowID = 0;
+        int colID = 0;
+        for (int i = 1; i < size - 1; i += 1) {
+            if (squares[i][colID] == 0) {
+                rowID = i;
+            }
+        }
+        newTiles[rowID][colID + 1] = 0; // move left
+        newTiles[rowID][colID] = squares[rowID][colID + 1];
+        neighbors[0] = new Board(newTiles);
+
+        newTiles[rowID][colID + 1] = squares[rowID][colID + 1]; // recover
+        newTiles[rowID - 1][colID] = 0; // move down
+        newTiles[rowID][colID] = squares[rowID - 1][colID];
+        neighbors[1] = new Board(newTiles);
+
+        newTiles[rowID - 1][colID] = squares[rowID - 1][colID];
+        newTiles[rowID + 1][colID] = 0; // move up
+        newTiles[rowID][colID] = squares[rowID + 1][colID];
+        neighbors[2] = new Board(newTiles);
+        return Arrays.asList(neighbors);
+    }
+
     private Boolean isRightSide() {
         for (int i = 1; i < size - 1; i += 1) {
             if (this.squares[i][size - 1] == 0) {
@@ -179,6 +382,78 @@ public final class Board {
             }
         }
         return false;
+    }
+
+    private Iterable<Board> rightSideNeighbors() {
+        Board[] neighbors = new Board[3];
+        int[][] newTiles = new int[size][size];
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                newTiles[i][j] = squares[i][j];
+            }
+        }
+        int rowID = 0;
+        int colID = size - 1;
+        for (int i = 1; i < size - 1; i += 1) {
+            if (squares[i][colID] == 0) {
+                rowID = i;
+            }
+        }
+        newTiles[rowID][colID - 1] = 0; // move right
+        newTiles[rowID][colID] = squares[rowID][colID - 1];
+        neighbors[0] = new Board(newTiles);
+
+        newTiles[rowID][colID - 1] = squares[rowID][colID - 1]; // recover
+        newTiles[rowID - 1][colID] = 0; // move down
+        newTiles[rowID][colID] = squares[rowID - 1][colID];
+        neighbors[1] = new Board(newTiles);
+
+        newTiles[rowID - 1][colID] = squares[rowID - 1][colID]; // recover
+        newTiles[rowID + 1][colID] = 0; // move up
+        newTiles[rowID][colID] = squares[rowID + 1][colID];
+        neighbors[2] = new Board(newTiles);
+        return Arrays.asList(neighbors);
+    }
+
+    private Iterable<Board> innerNeighbors() {
+        Board[] neighbors = new Board[4];
+        int[][] newTiles = new int[size][size];
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                newTiles[i][j] = squares[i][j];
+            }
+        }
+        int rowID = 0;
+        int colID = 0;
+        for (int i = 1; i < size - 1; i += 1) {
+            for (int j = 1; j < size - 1; j += 1) {
+                if (squares[i][j] == 0) {
+                    rowID = i;
+                    colID = j;
+                }
+            }
+        }
+
+        newTiles[rowID][colID - 1] = 0; // move right
+        newTiles[rowID][colID] = squares[rowID][colID - 1];
+        neighbors[0] = new Board(newTiles);
+
+        newTiles[rowID][colID - 1] = squares[rowID][colID - 1]; // recover
+        newTiles[rowID - 1][colID] = 0; // move down
+        newTiles[rowID][colID] = squares[rowID - 1][colID];
+        neighbors[1] = new Board(newTiles);
+
+        newTiles[rowID - 1][colID] = squares[rowID - 1][colID]; // recover
+        newTiles[rowID + 1][colID] = 0; // move up
+        newTiles[rowID][colID] = squares[rowID + 1][colID];
+        neighbors[2] = new Board(newTiles);
+
+        newTiles[rowID + 1][colID] = squares[rowID + 1][colID]; // recover
+        newTiles[rowID][colID + 1] = 0; // move left
+        newTiles[rowID][colID] = squares[rowID][colID + 1];
+        neighbors[3] = new Board(newTiles);
+
+        return Arrays.asList(neighbors);
     }
 
     private Boolean isInnerTile() {
@@ -196,9 +471,18 @@ public final class Board {
 
 
     public static void main(String[] args) {
-        // int[][] data = new int[][] { { 1, 2, 3 }, { 6, 5, 7 }, { 8, 2, 9 } };
-        // Board B = new Board(data);
-        // System.out.println(B.toString());
-
+        // int[][] data1 = new int[][] { { 1, 2, 3 }, { 6, 5, 7 }, { 8, 0, 4 } };
+        // Board B1 = new Board(data1);
+        // System.out.println(B1.toString());
+        // System.out.println(B1.hamming());
+        // System.out.println(B1.manhattan());
+        // System.out.println(B1.twin());
+        //
+        // int[][] data2 = new int[][] { { 8, 1, 3 }, { 4, 0, 2 }, { 7, 6, 5 } };
+        // Board B2 = new Board(data2);
+        // System.out.println(B2.toString());
+        // System.out.println(B2.hamming());
+        // System.out.println(B2.manhattan());
+        // System.out.println(B2.twin());
     }
 }

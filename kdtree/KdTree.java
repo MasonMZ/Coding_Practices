7c8d06784efd7ad25071d426474cb5b0054160c0
Mason <minzhengcsu@gmail.com>
2022-12-02 09:08:47 -0500

    add insert function

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -7,21 +7,78 @@
 import edu.princeton.cs.algs4.Point2D;
 
 public class KdTree {
+    private Node root;
+    private int size;
 
 
-    private class kdBST {
-        private class Node {
-            private Point2D point;
-            private Node left, right;
-            private boolean isKeyX;
+    private static class Node {
+        private Point2D point;
+        private Node left, right;
+        private Node parent;
+        private boolean isKeyX;
 
-            public Node(Point2D p, Boolean preKeyIsX) {
-                point = p;
-                isKeyX = !preKeyIsX;
+        public Node(Point2D p, Boolean preKeyIsX, Node pre) {
+            point = p;
+            isKeyX = !preKeyIsX;
+            parent = pre;
+        }
+    }
+
+    /* construct an empty set of points **/
+    public KdTree() {
+        root = null;
+        size = 0;
+    }
+
+    /* is the set empty?  **/
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /* number of points in the set  **/
+    public int size() {
+        return size;
+    }
+
+    /* add the point to the set (if it is not already in the set)  **/
+    public void insert(Point2D p) {
+        root = put(root, p, null);
+    }
+
+    private Node put(Node n, Point2D p, Node pre) {
+        if (n == null) {
+            if (root == null) {
+                return new Node(p, false, null);
+            }
+            else {
+                return new Node(p, pre.isKeyX, pre);
             }
         }
-
-        private Node root;
+        if (n.isKeyX) {
+            if (p.x() < n.point.x()) {
+                put(n.left, p, n);
+            }
+            else if (p.x() > n.point.x()) {
+                put(n.right, p, n);
+            }
+            else {
+                if (p.y() != n.point.y()) {
+                    put(n.right, p, n);
+                }
+            }
+        }
+        else {
+            if (p.y() < n.point.y()) {
+                put(n.left, p, n);
+            }
+            else if (p.y() > n.point.y()) {
+                put(n.right, p, n);
+            }
+            else if (p.x() != n.point.x()) {
+                put(n.right, p, n);
+            }
+        }
+        return n;
     }
 
 

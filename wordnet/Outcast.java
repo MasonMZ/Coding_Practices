d506458d4d08504f7650a04a4f240d5c569ee458
Mason <minzhengcsu@gmail.com>
2022-12-11 09:48:43 -0500

    add Outcast

--- wordnet/Outcast.java
+++ wordnet/Outcast.java
@@ -4,8 +4,49 @@
  *  Description:
  **************************************************************************** */
 
-public class Outcast {
-    public static void main(String[] args) {
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 
+import java.util.TreeSet;
+
+public class Outcast {
+    private WordNet w;
+
+    // constructor takes a WordNet object
+    public Outcast(WordNet wordnet) {
+        w = wordnet;
+    }
+
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns) {
+        int distance = -1;
+        String candidate = "";
+        TreeSet<String> auxTree = new TreeSet<>();
+        for (String s : nouns) {
+            if (w.isNoun(s)) {
+                auxTree.add(s);
+            }
+        }
+        for (String i : auxTree) {
+            int d = 0;
+            for (String j : auxTree) {
+                d += w.distance(i, j);
+            }
+            if (d > distance) {
+                distance = d;
+                candidate = i;
+            }
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        Outcast outcast = new Outcast(wordnet);
+        for (int t = 2; t < args.length; t++) {
+            In in = new In(args[t]);
+            String[] nouns = in.readAllStrings();
+            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+        }
     }
 }

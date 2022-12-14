65979d04c4e9032f9241f68c0fbc8ba47e57569b
Mason <minzhengcsu@gmail.com>
2022-12-14 16:47:53 -0500

    modify enegy by change get to getRGB function

--- SeamCarving/SeamCarver.java
+++ SeamCarving/SeamCarver.java
@@ -57,31 +57,39 @@ public class SeamCarver {
     private double xGradientSquared(int x, int y) {
         int left = x - 1;
         int right = x + 1;
+        int rgbLeft = workingPic.getRGB(left, y);
+        int rgbRight = workingPic.getRGB(right, y);
         double r_xSquared = Math.pow(
-                ((double) workingPic.get(left, y).getRed() - workingPic.get(right, y).getRed()), 2);
+                ((double) ((rgbLeft >> 16) & 0xFF) - ((rgbRight >> 16) & 0xFF)), 2);
         double g_xSquared = Math.pow(
-                ((double) workingPic.get(left, y).getGreen() - workingPic.get(right, y).getGreen()),
-                2);
+                ((double) ((rgbLeft >> 8) & 0xFF) - ((rgbRight >> 8) & 0xFF)), 2);
         double b_xSquared = Math.pow(
-                ((double) workingPic.get(left, y).getBlue() - workingPic.get(right, y).getBlue()),
-                2);
+                ((double) ((rgbLeft >> 0) & 0xFF) - ((rgbRight >> 0) & 0xFF)), 2);
         return r_xSquared + g_xSquared + b_xSquared;
     }
 
     private double yGradientSquared(int x, int y) {
         int upside = y - 1;
         int downside = y + 1;
+        int rgbUp = workingPic.getRGB(x, upside);
+        int rgbDown = workingPic.getRGB(x, downside);
         double r_ySquared = Math.pow(
-                ((double) workingPic.get(x, upside).getRed() - workingPic.get(x, downside)
-                                                                         .getRed()), 2);
+                ((double) ((rgbUp >> 16) & 0xFF) - ((rgbDown >> 16) & 0xFF)), 2);
         double g_ySquared = Math.pow(
-                ((double) workingPic.get(x, upside).getGreen() - workingPic.get(x, downside)
-                                                                           .getGreen()),
-                2);
+                ((double) ((rgbUp >> 8) & 0xFF) - ((rgbDown >> 8) & 0xFF)), 2);
         double b_ySquared = Math.pow(
-                ((double) workingPic.get(x, upside).getBlue() - workingPic.get(x, downside)
-                                                                          .getBlue()),
-                2);
+                ((double) ((rgbUp >> 0) & 0xFF) - ((rgbDown >> 0) & 0xFF)), 2);
+        // double r_ySquared = Math.pow(
+        //         ((double) workingPic.get(x, upside).getRed() - workingPic.get(x, downside)
+        //                                                                  .getRed()), 2);
+        // double g_ySquared = Math.pow(
+        //         ((double) workingPic.get(x, upside).getGreen() - workingPic.get(x, downside)
+        //                                                                    .getGreen()),
+        //         2);
+        // double b_ySquared = Math.pow(
+        //         ((double) workingPic.get(x, upside).getBlue() - workingPic.get(x, downside)
+        //                                                                   .getBlue()),
+        //         2);
         return r_ySquared + g_ySquared + b_ySquared;
     }
 
@@ -97,7 +105,39 @@ public class SeamCarver {
 
     private void pic2Graph() {
         workingGraph = new Digraph(width() * (height() - 2) + 2);
+        for (int i = 0; i < width(); i += 1) {
+            workingGraph.addEdge(0, i + 1);
+            workingGraph.addEdge(width() * (height() - 3) + i, width() * (height() - 2) + 1);
+        }
+        for (int row = 0; row < (height() - 3); row += 1) {
+            for (int col = 0; col < width(); col += 1) {
+                connect(workingGraph, col, row);
+            }
+        }
+    }
 
+    private void connect(Digraph d, int x, int y) {
+        int target = y * width() + x + 1;
+        if (x == 0) {
+            int targetDownSide = (y + 1) * width() + x + 1;
+            int targetLowerRight = (y + 1) * width() + x + 2;
+            d.addEdge(target, targetDownSide);
+            d.addEdge(target, targetLowerRight);
+        }
+        else if (x == width() - 1) {
+            int targetDownSide = (y + 1) * width() + x + 1;
+            int targetLowerLeft = (y + 1) * width() + x;
+            d.addEdge(target, targetDownSide);
+            d.addEdge(target, targetLowerLeft);
+        }
+        else {
+            int targetLowerLeft = (y + 1) * width() + x;
+            int targetDownSide = (y + 1) * width() + x + 1;
+            int targetLowerRight = (y + 1) * width() + x + 2;
+            d.addEdge(target, targetLowerLeft);
+            d.addEdge(target, targetDownSide);
+            d.addEdge(target, targetLowerRight);
+        }
     }
 
 

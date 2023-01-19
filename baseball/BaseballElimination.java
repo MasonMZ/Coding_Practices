481d094c4488e3dfaed7c82d494e3004fd6d541d
Mason <minzhengcsu@gmail.com>
2023-01-19 10:53:19 -0500

    add helper fn to implement isEliminated

--- baseball/BaseballElimination.java
+++ baseball/BaseballElimination.java
@@ -4,8 +4,11 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.FlowEdge;
+import edu.princeton.cs.algs4.FlowNetwork;
 import edu.princeton.cs.algs4.In;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -14,8 +17,10 @@ public class BaseballElimination {
     private int[] wins;
     private int[] losses;
     private int[] remainingGames;
-    private int[][] g;
+    private int[][] g; // num of remaining games between two teams i and j
+    private ArrayList<Integer>[] opponents;
     private HashMap<String, Integer> teamIdPair;
+    private ArrayList<String>[] coe;
 
     /** create a baseball division from given filename */
     public BaseballElimination(String filename) {
@@ -42,8 +47,12 @@ public class BaseballElimination {
             wins[count] = Integer.parseInt(fields[1]);
             losses[count] = Integer.parseInt(fields[2]);
             remainingGames[count] = Integer.parseInt(fields[3]);
+            opponents = new ArrayList<>[teamNum];
             for (int i = 0; i < teamNum; i += 1) {
                 g[count][i] = Integer.parseInt(fields[4 + i]);
+                for (int j = i + 1; j < teamNum; j += 1) {
+                    opponents[i].add(j);
+                }
             }
             count += 1;
         }
@@ -90,6 +99,58 @@ public class BaseballElimination {
         }
         return g[teamIdPair.get(team1)][teamIdPair.get(team2)];
     }
+
+    /** Helper function to get coe */
+    private void getCoe(String team) {
+        int n = teams.length;
+        int currTeamNum = teamIdPair.get(team);
+        if (coe[currTeamNum] != null) {
+            return;
+        }
+        for (int i = 0; i < n; i += 1) {
+            if (i == currTeamNum) {
+                continue;
+            }
+            if (wins[i] > (wins[currTeamNum] + remainingGames[currTeamNum])) {
+                coe[currTeamNum].add(teams[i]);
+                return;
+            }
+        }
+        int verticeNum = (n - 1) * (n - 2) / 2 + n + 1;
+        int source = (n - 1) * (n - 2) / 2 + n - 1;
+        int target = (n - 1) * (n - 2) / 2 + n;
+        FlowNetwork f = new FlowNetwork(verticeNum);
+        int matchCount = 0;
+        for (int i = 0; i < currTeamNum; i += 1) {
+            f.addEdge(new FlowEdge(i, target, wins[i] + remainingGames[i] - wins[currTeamNum]));
+            for (int j = 0; j < opponents[i].size(); j += 1) {
+                f.addEdge(new FlowEdge(source, n - 1 + matchCount, g[i][j]));
+                f.addEdge(new FlowEdge(n - 1 + matchCount, i, Double.POSITIVE_INFINITY));
+                matchCount += 1;
+            }
+        }
+        for (int i = currTeamNum; i < n - 1; i += 1) {
+            f.addEdge(new FlowEdge(i, target,
+                                   wins[i + 1] + remainingGames[i + 1] - wins[currTeamNum]));
+            for (int j = 0; j < opponents[i + 1].size(); j += 1) {
+                f.addEdge(new FlowEdge(source, n - 1 + matchCount, g[i + 1][j]));
+                f.addEdge(new FlowEdge(n - 1 + matchCount, i, Double.POSITIVE_INFINITY));
+                matchCount += 1;
+            }
+        }
+
+    }
+
+    /** is given team eliminated? */
+    public boolean isEliminated(String team) {
+        if (!teamIdPair.containsKey(team)) {
+            throw new IllegalArgumentException("The team is not in the division!");
+        }
+        getCoe(team);
+        int currTeamNum = teamIdPair.get(team);
+        return !(coe[currTeamNum] == null);
+    }
+
 
     public static void main(String[] args) {
         // BaseballElimination division = new BaseballElimination("teams4.txt");
